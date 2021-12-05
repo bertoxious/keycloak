938f373e96704bed3478d443f01f2c90e82beff0
Ashish <ashishdev@zohomail.in>
2021-12-05 15:10:01 +0530

    Added JwtAuthenticationConverter

--- resource-server-with-spring-boot/ResourceServer/src/main/java/com/ashish/ResourceServer/security/WebSecurity.java
+++ resource-server-with-spring-boot/ResourceServer/src/main/java/com/ashish/ResourceServer/security/WebSecurity.java
@@ -4,15 +4,19 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
         http.authorizeRequests().antMatchers(HttpMethod.GET,"/users/status/check")
 //                .hasAuthority("SCOPE_profile")
                 .hasRole("developer") //will check for the specific role
 //                .hasAnyRole("role_one", "role_two") // will check for any role and we can provide as many roles
-        .anyRequest().authenticated().and().oauth2ResourceServer().jwt();
+        .anyRequest().authenticated().and().oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
 }
