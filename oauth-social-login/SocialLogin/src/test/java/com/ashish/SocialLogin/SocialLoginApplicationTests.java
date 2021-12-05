3a17a326ec4302e50654df555de22f52f8ae4ec4
Ashish <ashishdev@zohomail.in>
2021-12-05 18:49:00 +0530

    Social Login App Template

---
+++ oauth-social-login/SocialLogin/src/test/java/com/ashish/SocialLogin/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashish.SocialLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
