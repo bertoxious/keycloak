10823156c40d4b6f352c0a1c3e2e6a4112a39963
Ashish <ashishdev@zohomail.in>
2021-12-06 10:03:51 +0530

    Added Succesfull logout completely

--- oauth-social-login/SocialLogin/src/main/java/com/ashish/SocialLogin/security/WebSecurity.java
+++ oauth-social-login/SocialLogin/src/main/java/com/ashish/SocialLogin/security/WebSecurity.java
@@ -1,12 +1,17 @@
 package com.ashish.SocialLogin.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    ClientRegistrationRepository clientRegistrationRepository;
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
@@ -14,9 +19,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated().and().oauth2Login()
                 .and()
                 .logout()
-                .logoutSuccessUrl("/great")
+//                .logoutSuccessUrl("/great")
+                .logoutSuccessHandler(oidcLogoutSuccessHandler())
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
                 .deleteCookies("JSESSIONID");
+    }
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler(){
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("http://localhost:8080");
+        return successHandler;
     }
 }
